117b04c6eac9c991c46dd645f2a60a2b563c4cbd
Alexander Pronin <pronin.alx@gmail.com>
2015-11-12 16:22:48 +0200

    Initial Commit

---
+++ ZazoVIPERTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZazoVIPERTest
+//
+//  Created by Aleksandr Pronin on 11/12/15.
+//  Copyright © 2015 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
